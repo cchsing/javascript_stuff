68cefde15149ebfb5e56c096f6ef72aab08f53f4
cchsing <jcch950607@gmail.com>
2022-12-04 22:26:39 +0800

    updates

---
+++ requireJS/config/config_0.js
@@ -0,0 +1,16 @@
+requirejs.config({
+    baseUrl: 'lib',
+    paths: {
+        angular: [
+            'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.8.3/angular.min',
+            'angular.min'
+        ],
+        extCore: 'ext-core',
+        jquery: [
+            'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min',
+            'jquery-3.6.1.min'
+        ],
+        mootools: 'MooTools-Core-1.6.0',
+        swfObj: 'swjobject'
+    }
+});
---
+++ requireJS/scripts/customScript1.js
@@ -0,0 +1,7 @@
+define([
+    'jquery',
+    'angular',
+    'extCore'
+], function($,angular,extCore) {
+    $('body').html('Testing Testing Testing');
+});
